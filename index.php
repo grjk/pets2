a04440ec85755457f831d92ae3ab6304fa7a19de
Gorbataras <gorbataras@gmail.com>
2020-01-24 13:47:25 -0500

    final commit

--- index.php
+++ index.php
@@ -18,6 +18,29 @@ $f3 -> route("GET /", function () {
     echo $view->render("views/home.html");
 });
 
+// Define a default route (view)
+$f3 -> route("GET /order", function () {
+    $view = new Template();
+
+    echo $view->render("views/form1.html");
+});
+
+// Define a default route (view)
+$f3 -> route("POST /order2", function () {
+    $view = new Template();
+    $_SESSION['animal'] = $_POST['animal'];
+    echo $view->render("views/form2.html");
+});
+
+
+$f3 -> route("POST /results", function () {
+    $view = new Template();
+    $_SESSION['color'] = $_POST['color'];
+    echo $view->render("views/results.html");
+});
+
+
+
 $f3->route('GET /@animal',function ($f3, $params) {
 //    var_dump($params);
     $animal = $params['animal'];
@@ -34,6 +57,7 @@ $f3->route('GET /@animal',function ($f3, $params) {
     } else {
         $f3->error(404);
     }
+
 
 });
 
